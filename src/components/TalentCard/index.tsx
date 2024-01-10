89b363736c500a953f8e3b7fea8adc35c57d541a
stephane0713 <stephaned2205@gmail.com>
2024-01-10 11:38:57 +0100

    #11 terminer; Ajout de RouterProvider;

---
+++ src/components/TalentCard/index.tsx
@@ -0,0 +1,54 @@
+import { Box, Link, Typography } from "@mui/material";
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+interface TalentCardProps {
+  talentId: number;
+  talentName: string;
+  talentProfession: string;
+  alt: string;
+  src: string;
+}
+
+const TalentCard = ({
+  talentId,
+  talentName,
+  talentProfession,
+  alt,
+  src,
+}: TalentCardProps) => {
+  const textSx = {
+    color: "#333",
+    textDecorationColor: "#777",
+    fontSize: "0.9rem",
+  };
+
+  return (
+    <Box sx={{}}>
+      <Box
+        component="img"
+        alt={alt}
+        src={src}
+        sx={{
+          boxShadow: 3,
+          aspectRatio: "7/8",
+          mb: 1,
+        }}
+      />
+      <Typography component="span" fontWeight="700" sx={{ ...textSx }}>
+        {talentName}{" "}
+      </Typography>
+      <Typography component="span" fontWeight="600">
+        <Link
+          component={RouterLink}
+          to={`/talents/${talentId}`}
+          sx={{ ...textSx }}
+        >
+          {talentProfession}
+        </Link>
+      </Typography>
+    </Box>
+  );
+};
+
+export default TalentCard;
