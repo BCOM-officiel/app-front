b77bc0c30265ee83a79747a0ffb74ea9a860451d
Nicolas-B06 <bernardnicolas06600@gmail.com>
2024-01-24 17:08:12 +0100

    card member team v1 waiting for final img members

---
+++ src/components/TeamMemberCard/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, CardContent, Typography, Avatar, CardMedia } from '@mui/material';
+import micka from '../../assets/micka.png';
+
+type ProfileCardProps = {
+  firstName: string;
+  profession: string;
+  imageUrl: string;
+  color: string;
+  textColor: string;
+};
+
+const TeamMemberCard: React.FC<ProfileCardProps> = ({ firstName, profession, imageUrl, color, textColor }) => {
+  return (
+    <Card sx={{ maxWidth: 300, backgroundColor: color }}>
+     
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" sx={{marginTop: 10 , color:`${textColor}`}}>
+          {firstName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{color: `${textColor}`, textDecoration: "underline"}}>
+          {profession}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        height="400"
+        image={micka}
+        alt={`${firstName}`}
+        sx={{ marginTop: 12 }}
+      />
+    </Card>
+  );
+};
+
+export default TeamMemberCard;
